ddfc4d7816bf485d51c6c3df0081a6dfeeeec64b
fFukurou <supevi1@gmail.com>
2025-11-11 20:54:19 -0300

    added initial files

---
+++ src/main/java/com/nebulohub/NebulohubApplication.java
@@ -0,0 +1,13 @@
+package com.nebulohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nebulohub/NebulohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebulohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
